92e14e171d7aa48b6f3b25bfb60cd70cc831cfa0
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-28 21:21:23 +0530

    [Rituparna]Added: Pincode Not Starting With Char

--- src/com/bridgelabs/pincode/Regex.java
+++ src/com/bridgelabs/pincode/Regex.java
@@ -8,15 +8,17 @@ public class Regex {
     { 
   
 		System.out.println("Welcome to Pincode problem");
-        String regex = "[0-9]{6}"; 
+        String regex = "^[0-9]{6}"; 
         String pincode = "400088"; 
-        
+        String pincode1 = "A400088"; 
         
         Pattern pattern = Pattern.compile(regex); 
   
         
         
         Matcher matcher = pattern.matcher(pincode); 
+        Matcher matcher1 = pattern.matcher(pincode1);
         System.out.println("Pattern match is: " + matcher.find()); 
+        System.out.println("Pattern match is: " + matcher1.find()); 
     } 
 }

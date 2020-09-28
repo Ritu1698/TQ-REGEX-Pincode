bc9c2984608760f57a6d7eb355ad0762c3eeb785
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-28 21:23:44 +0530

    [Rituparna]Added: Pincode Not Ending With Char

--- src/com/bridgelabs/pincode/Regex.java
+++ src/com/bridgelabs/pincode/Regex.java
@@ -8,9 +8,10 @@ public class Regex {
     { 
   
 		System.out.println("Welcome to Pincode problem");
-        String regex = "^[0-9]{6}"; 
+        String regex = "^[0-9]{6}$"; 
         String pincode = "400088"; 
-        String pincode1 = "A400088"; 
+        String pincode1 = "A400088";
+        String pincode2 = "400088B";
         
         Pattern pattern = Pattern.compile(regex); 
   
@@ -18,7 +19,9 @@ public class Regex {
         
         Matcher matcher = pattern.matcher(pincode); 
         Matcher matcher1 = pattern.matcher(pincode1);
+        Matcher matcher2 = pattern.matcher(pincode2);
         System.out.println("Pattern match is: " + matcher.find()); 
-        System.out.println("Pattern match is: " + matcher1.find()); 
+        System.out.println("Pattern match is: " + matcher1.find());
+        System.out.println("Pattern match is: " + matcher2.find());
     } 
 }

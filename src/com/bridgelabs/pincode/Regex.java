981d3f4088498e779ed5eefed0ed6678fb917105
Ritu1698 <biswasrituparna83@gmail.com>
2020-09-28 21:27:03 +0530

    [Rituparna]Added: Pincode with Space in Between

--- src/com/bridgelabs/pincode/Regex.java
+++ src/com/bridgelabs/pincode/Regex.java
@@ -8,10 +8,11 @@ public class Regex {
     { 
   
 		System.out.println("Welcome to Pincode problem");
-        String regex = "^[0-9]{6}$"; 
+        String regex = "^[0-9]{3}(\\s{0,1})[0-9]{3}$"; 
         String pincode = "400088"; 
         String pincode1 = "A400088";
         String pincode2 = "400088B";
+        String pincode3 = "400 088";
         
         Pattern pattern = Pattern.compile(regex); 
   
@@ -20,8 +21,10 @@ public class Regex {
         Matcher matcher = pattern.matcher(pincode); 
         Matcher matcher1 = pattern.matcher(pincode1);
         Matcher matcher2 = pattern.matcher(pincode2);
+        Matcher matcher3 = pattern.matcher(pincode3);
         System.out.println("Pattern match is: " + matcher.find()); 
         System.out.println("Pattern match is: " + matcher1.find());
         System.out.println("Pattern match is: " + matcher2.find());
+        System.out.println("Pattern match is: " + matcher3.find());
     } 
 }
